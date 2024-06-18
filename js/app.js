8c1b33037809cfd2a576a15132226c8678752e7b
pratitb <pratit.bangdiwala7@gmail.com>
2024-06-19 01:06:33 +0530

    first commit

---
+++ js/app.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+	$('.category').on('click', function () {
+		// highlight selected category
+		$('.category').removeClass('category_active');
+		$(this).addClass('category_active');
+
+		// get clicked category name
+		const category = $(this).data('name');
+		const selectedCategories = category.split(',');
+
+		// find all projects w category
+		$('.project').each(function () {
+			const projectType = $(this).data('type').split(',');
+			let showProject = false;
+
+			for (const pType of projectType) {
+				if (selectedCategories.includes(pType)) {
+					showProject = true;
+					break;
+				}
+			}
+			if (showProject) {
+				$(this).addClass('show');
+			} else {
+				$(this).removeClass('show');
+			}
+		});
+	});
+});
